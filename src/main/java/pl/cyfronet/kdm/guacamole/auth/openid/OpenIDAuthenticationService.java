c1639d513784d80dfd0ec14ebae4e6a61a6f3d8e
Maciej Pawlik <m.pawlik@cyfronet.pl>
2016-12-09 15:41:23 +0100

    Add openid4java dependecy

--- src/main/java/pl/cyfronet/kdm/guacamole/auth/openid/OpenIDAuthenticationService.java
+++ src/main/java/pl/cyfronet/kdm/guacamole/auth/openid/OpenIDAuthenticationService.java
@@ -6,6 +6,10 @@ import org.glyptodon.guacamole.net.auth.AuthenticatedUser;
 import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.net.auth.credentials.CredentialsInfo;
 import org.glyptodon.guacamole.net.auth.credentials.GuacamoleInvalidCredentialsException;
+import org.openid4java.association.AssociationSessionType;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.InMemoryConsumerAssociationStore;
+import org.openid4java.consumer.InMemoryNonceVerifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.cyfronet.kdm.guacamole.auth.openid.form.OpenIDField;
@@ -16,7 +20,19 @@ public class OpenIDAuthenticationService {
 
     private static final Logger logger = LoggerFactory.getLogger(OpenIDAuthenticationService.class);
 
+    private ConsumerManager consumerManager;
+
+
+    public OpenIDAuthenticationService() {
+        this.consumerManager = new ConsumerManager();
+        consumerManager.setAssociations(new InMemoryConsumerAssociationStore());
+        consumerManager.setNonceVerifier(new InMemoryNonceVerifier(5000));
+        consumerManager.setMinAssocSessEnc(AssociationSessionType.DH_SHA256);
+    }
+
     public AuthenticatedUser authenticateUser(Credentials credentials) throws GuacamoleException {
+        logger.debug("authenticateUser()");
+        //put openid code here, check for proper request if it's invalid throw exception
 
         throw new GuacamoleInvalidCredentialsException("Invalid login",
                 new CredentialsInfo(Arrays.asList(new Field[]{
