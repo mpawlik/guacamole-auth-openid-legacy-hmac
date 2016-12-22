0d7ce182fa2c50722843308a7699ce4615bfb186
Maciej Pawlik <m.pawlik@cyfronet.pl>
2016-12-22 17:46:27 +0100

    Add basing openid integration.

--- src/main/java/pl/cyfronet/kdm/guacamole/auth/openid/OpenIDAuthenticationProvider.java
+++ src/main/java/pl/cyfronet/kdm/guacamole/auth/openid/OpenIDAuthenticationProvider.java
@@ -5,8 +5,11 @@ import org.glyptodon.guacamole.net.auth.AuthenticatedUser;
 import org.glyptodon.guacamole.net.auth.AuthenticationProvider;
 import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.net.auth.UserContext;
+import org.openid4java.discovery.DiscoveryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 public class OpenIDAuthenticationProvider implements AuthenticationProvider {
@@ -20,7 +23,7 @@ public class OpenIDAuthenticationProvider implements AuthenticationProvider {
     public AuthenticatedUser authenticateUser(Credentials credentials) throws GuacamoleException {
         logger.debug("authenticateUser()");
 
-        OpenIDAuthenticationService openIDAuthenticationService = new OpenIDAuthenticationService();
+        OpenIDAuthenticationService openIDAuthenticationService = new OpenIDAuthenticationService(credentials.getRequest());
         return openIDAuthenticationService.authenticateUser(credentials);
     }
 
--- src/main/java/pl/cyfronet/kdm/guacamole/auth/openid/OpenIDAuthenticationService.java
+++ src/main/java/pl/cyfronet/kdm/guacamole/auth/openid/OpenIDAuthenticationService.java
@@ -7,23 +7,37 @@ import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.net.auth.credentials.CredentialsInfo;
 import org.glyptodon.guacamole.net.auth.credentials.GuacamoleInvalidCredentialsException;
 import org.openid4java.association.AssociationSessionType;
+import org.openid4java.consumer.ConsumerException;
 import org.openid4java.consumer.ConsumerManager;
 import org.openid4java.consumer.InMemoryConsumerAssociationStore;
 import org.openid4java.consumer.InMemoryNonceVerifier;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.MessageException;
+import org.openid4java.message.ax.FetchRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.cyfronet.kdm.guacamole.auth.openid.form.OpenIDField;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
+import java.util.List;
 
 public class OpenIDAuthenticationService {
 
     private static final Logger logger = LoggerFactory.getLogger(OpenIDAuthenticationService.class);
+    private static final String CONSUMER_MANAGER = "openid_consumer_manager";
+    private static final String returnToUrl = "https://viz.pro.cyfronet.pl/screen/";
 
-    private ConsumerManager consumerManager;
+
+    private static ConsumerManager consumerManager;
+    private static DiscoveryInformation discovered;
 
 
-    public OpenIDAuthenticationService() {
+    public OpenIDAuthenticationService(HttpServletRequest request) {
+
+        logger.debug("create new ConsumerManager");
         this.consumerManager = new ConsumerManager();
         consumerManager.setAssociations(new InMemoryConsumerAssociationStore());
         consumerManager.setNonceVerifier(new InMemoryNonceVerifier(5000));
@@ -33,15 +47,35 @@ public class OpenIDAuthenticationService {
     public AuthenticatedUser authenticateUser(Credentials credentials) throws GuacamoleException {
         logger.debug("authenticateUser()");
         //put openid code here, check for proper request if it's invalid throw exception
+        List discoveries = null;
+        try {
+            discoveries = consumerManager.discover("https://openid.plgrid.pl/gateway");
+            discovered = consumerManager.associate(discoveries);
 
-        throw new GuacamoleInvalidCredentialsException("Invalid login",
-                new CredentialsInfo(Arrays.asList(new Field[]{
-                        new OpenIDField(
-                                "https://openid.plgrid.pl"
-                        )
-                }))
-        );
+            FetchRequest fetch = FetchRequest.createFetchRequest();
+            fetch.addAttribute("email", "http://schema.openid.net/contact/email", true);
 
+            AuthRequest authReq = consumerManager.authenticate(discovered, returnToUrl);
+            authReq.addExtension(fetch);
+
+            String destinationUrl = authReq.getDestinationUrl(true);
+            logger.debug("destinationURL: {}", destinationUrl);
+            throw new GuacamoleInvalidCredentialsException("Invalid login",
+                    new CredentialsInfo(Arrays.asList(new Field[]{
+                            new OpenIDField(
+                                    destinationUrl
+                            )
+                    }))
+            );
+        } catch (DiscoveryException e) {
+            e.printStackTrace();
+        } catch (ConsumerException e) {
+            e.printStackTrace();
+        } catch (MessageException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
